73cd489866be192253a3ca2f1f70d6703106732f
John Nolette <jnolette@transparent.com>
2019-05-17 21:43:29 -0400

    Fixing npm scripts

--- src/tailwind.js
+++ src/tailwind.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     screens: {
       tablet: '768px',
-      desktop: '1024px',
+      desktop: '1024px'
     },
     colors: {
       primary: {
@@ -10,15 +10,15 @@ module.exports = {
         300: '#90cdf4',
         500: '#4299e1',
         700: '#2b6cb0',
-        900: '#2a4365',
+        900: '#2a4365'
       },
       secondary: {
         100: '#fffff0',
         300: '#faf089',
         500: '#ecc94b',
         700: '#b7791f',
-        900: '#744210',
-      },
+        900: '#744210'
+      }
     },
     extend: {
       boxShadow: {
