29d5808e79268dd3e031ce6709a2a00bf47f1d21
John Nolette <jnolette@transparent.com>
2019-05-17 21:04:03 -0400

    Adding webpack plugins, tailwind config, browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,51 @@
+const autoprefixer = require('autoprefixer');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
+  entry: {
+    main: './src/app.js'
+  },
+  output: {
+    filename: 'metatube.bundle.js',
+    publicPath: './'
+  },
+  watch: false,
+  mode: 'development',
+  plugins: [
+    new WebpackNotifierPlugin({ alwaysNotify: true }),
+
+    new MiniCssExtractPlugin({
+      fileame: "metatube.bundle.css"
+    }),
+
+    new OptimizeCSSAssetsPlugin({}),
+
+    new BrowserSyncPlugin({
+      proxy: 'mysite.local',
+      open: 'external',
+      host: 'mysite.local',
+      port: 3000,
+      files: ['./dist/main.css', './views', './tailwind.js']
+    },
+    {
+      reload: false
+    })
+  ],
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       {
         test: /\.riot$/,
         exclude: /node_modules/,
@@ -13,22 +58,37 @@ module.exports = {
         }]
       },
       {
-        test: /\.scss$/,
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
         use: [
           {
-            loader: "style-loader"
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader'
           },
           {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
-              includePaths: ["absolute/path/a", "absolute/path/b"]
+              plugins: () => [autoprefixer()]
             }
           }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'postcss-loader' }
         ]
       }
     ]
   }
-}
+};
