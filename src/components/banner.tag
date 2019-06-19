72cc2f516cde550e02cfe343f11ddceca1b87f53
John Nolette <jnolette@transparent.com>
2019-06-19 00:24:03 -0400

    Adding tailwind transitions

--- src/components/banner.tag
+++ src/components/banner.tag
@@ -10,7 +10,7 @@
       </form>
     </div>
   </div>
-  <button class="block m-auto bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+  <button class="btn">
     Button
   </button>
   <script>
--- src/tailwind.js
+++ src/tailwind.js
@@ -18,6 +18,49 @@ module.exports = {
         500: '#ecc94b',
         700: '#b7791f',
         900: '#744210'
+      },
+      transitionProperty: {
+        'none': 'none',
+        'all': 'all',
+        'color': 'color',
+        'bg': 'background-color',
+        'border': 'border-color',
+        'colors': ['color', 'background-color', 'border-color'],
+        'opacity': 'opacity',
+        'transform': 'transform',
+      },
+      transitionDuration: {
+        'default': '250ms',
+        '0': '0ms',
+        '100': '100ms',
+        '250': '250ms',
+        '500': '500ms',
+        '750': '750ms',
+        '1000': '1000ms',
+      },
+      transitionTimingFunction: {
+        'default': 'ease',
+        'linear': 'linear',
+        'ease': 'ease',
+        'ease-in': 'ease-in',
+        'ease-out': 'ease-out',
+        'ease-in-out': 'ease-in-out',
+      },
+      transitionDelay: {
+        'default': '0ms',
+        '0': '0ms',
+        '100': '100ms',
+        '250': '250ms',
+        '500': '500ms',
+        '750': '750ms',
+        '1000': '1000ms',
+      },
+      willChange: {
+        'auto': 'auto',
+        'scroll': 'scroll-position',
+        'contents': 'contents',
+        'opacity': 'opacity',
+        'transform': 'transform',
       }
     },
     extend: {
@@ -25,5 +68,15 @@ module.exports = {
         huge: '0 30px 60px -15px rgba(0, 0, 0, .25)'
       }
     }
-  }
+  },
+  variants: {
+    transitionProperty: ['responsive'],
+    transitionDuration: ['responsive'],
+    transitionTimingFunction: ['responsive'],
+    transitionDelay: ['responsive'],
+    willChange: ['responsive'],
+  },
+  plugins: [
+    require('tailwindcss-transitions')(),
+  ]
 }
