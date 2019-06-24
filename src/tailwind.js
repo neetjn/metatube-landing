5f3cf91df51e1110a7f467fcff266d94cf664a4b
John Nolette <jnolette@transparent.com>
2019-06-24 19:50:54 -0400

    Fixing transitions, cleaning up

--- src/tailwind.js
+++ src/tailwind.js
@@ -1,80 +1,15 @@
+
 module.exports = {
   theme: {
     screens: {
       tablet: '768px',
       desktop: '1024px'
     },
-    colors: {
-      primary: {
-        100: '#ebf8ff',
-        300: '#90cdf4',
-        500: '#4299e1',
-        700: '#2b6cb0',
-        900: '#2a4365'
-      },
-      secondary: {
-        100: '#fffff0',
-        300: '#faf089',
-        500: '#ecc94b',
-        700: '#b7791f',
-        900: '#744210'
-      },
-      transitionProperty: {
-        'none': 'none',
-        'all': 'all',
-        'color': 'color',
-        'bg': 'background-color',
-        'border': 'border-color',
-        'colors': ['color', 'background-color', 'border-color'],
-        'opacity': 'opacity',
-        'transform': 'transform',
-      },
-      transitionDuration: {
-        'default': '250ms',
-        '0': '0ms',
-        '100': '100ms',
-        '250': '250ms',
-        '500': '500ms',
-        '750': '750ms',
-        '1000': '1000ms',
-      },
-      transitionTimingFunction: {
-        'default': 'ease',
-        'linear': 'linear',
-        'ease': 'ease',
-        'ease-in': 'ease-in',
-        'ease-out': 'ease-out',
-        'ease-in-out': 'ease-in-out',
-      },
-      transitionDelay: {
-        'default': '0ms',
-        '0': '0ms',
-        '100': '100ms',
-        '250': '250ms',
-        '500': '500ms',
-        '750': '750ms',
-        '1000': '1000ms',
-      },
-      willChange: {
-        'auto': 'auto',
-        'scroll': 'scroll-position',
-        'contents': 'contents',
-        'opacity': 'opacity',
-        'transform': 'transform',
-      }
-    },
     extend: {
       boxShadow: {
         huge: '0 30px 60px -15px rgba(0, 0, 0, .25)'
       }
     }
-  },
-  variants: {
-    transitionProperty: ['responsive'],
-    transitionDuration: ['responsive'],
-    transitionTimingFunction: ['responsive'],
-    transitionDelay: ['responsive'],
-    willChange: ['responsive'],
   },
   plugins: [
     require('tailwindcss-transitions')(),
